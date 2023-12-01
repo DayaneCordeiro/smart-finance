e437d6ece25bbc66c20a7f8f39cbc08c66b14caa
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2023-12-01 16:24:56 -0300

    feat: base project

---
+++ src/test/java/com/example/smartfinance/SmartFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
