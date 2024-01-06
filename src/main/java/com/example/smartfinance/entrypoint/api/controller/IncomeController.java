8a0314dbd75dc85a026c3c780be9cf878c6e31cf
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2024-01-06 19:57:11 -0300

    fix: income create endpoint

--- src/main/java/com/example/smartfinance/entrypoint/api/controller/IncomeController.java
+++ src/main/java/com/example/smartfinance/entrypoint/api/controller/IncomeController.java
@@ -11,8 +11,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import static io.swagger.v3.oas.annotations.enums.ParameterIn.HEADER;
 
@@ -67,9 +74,9 @@ public interface IncomeController {
             )}
         )
     })
-    @GetMapping
+    @GetMapping("/{id}")
     IncomeOutputDTO get(
         @RequestHeader(value = "consumer-id") final String consumerId,
-        @Valid @RequestBody final String id
+        @PathVariable @NotBlank final String id
     );
 }
